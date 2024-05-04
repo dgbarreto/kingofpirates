d48d0c05931f0c06f2ed6fb690542d68e5ccb08d
Danilo Barreto <dgbarreto@gmail.com>
2024-05-04 18:25:28 -0300

    Initial commit

---
+++ KingOfPirates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KingOfPirates
+//
+//  Created by Danilo Barreto on 04/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
